a77a137745232277ffe3fefccb6d14a6e8953110
meldanurcelik <melda_celik@hotmail.com>
2023-03-05 19:59:00 +0300

    RestAssured ile PATCH Çağrısı

---
+++ src/test/java/com/otelrezervasyonu/PartiallyUpdateBookingTests.java
@@ -0,0 +1,34 @@
+package com.otelrezervasyonu;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class PartiallyUpdateBookingTests extends BaseTest {
+
+    @Test
+    public void partiallyUpdateBookingTest() {
+
+        String token = createToken();
+        int bookingId = createBookingId("Ozan", "Ilhan", 10, false);
+
+        JSONObject body = new JSONObject();
+        body.put("firstname", "Ayse");
+
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .header("Cookie", "token=" + token)
+                .body(body.toString())
+                .patch("https://restful-booker.herokuapp.com/booking/" + bookingId);
+
+        response.prettyPrint();
+
+        Assertions.assertEquals("Ayse", response.jsonPath().getJsonObject("firstname"));
+
+    }
+
+}
