6e99907f1e954d3d4f6dde25046eb5b26e010d01
meldanurcelik <melda_celik@hotmail.com>
2023-03-05 20:03:23 +0300

    RestAssured ile DELETE Çağrısı

---
+++ src/test/java/com/otelrezervasyonu/DeleteBookingTests.java
@@ -0,0 +1,30 @@
+package com.otelrezervasyonu;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class DeleteBookingTests extends BaseTest {
+
+    @Test
+    public void deleteBookingTest() {
+
+        String token = createToken();
+        int bookingId = createBookingId("Ozan", "Ilhan", 5, true);
+
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .header("Cookie", "token=" + token)
+                .delete("https://restful-booker.herokuapp.com/booking/" + bookingId);
+
+        response
+                .then()
+                .statusCode(201);
+
+        response.prettyPrint();
+
+    }
+
+}
