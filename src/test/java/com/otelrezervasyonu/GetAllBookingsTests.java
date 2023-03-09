78f3895a71b2383be799efeea1f1d1b659e0d52f
meldanurcelik <melda_celik@hotmail.com>
2023-03-09 21:53:50 +0300

    Path ve Query Parametrelerini Projeye Ekleme

--- src/test/java/com/otelrezervasyonu/GetAllBookingsTests.java
+++ src/test/java/com/otelrezervasyonu/GetAllBookingsTests.java
@@ -1,6 +1,10 @@
 package com.otelrezervasyonu;
 
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
@@ -15,6 +19,26 @@ public class GetAllBookingsTests extends BaseTest {
                 .then()
                 //.log().all()
                 .statusCode(200);
+
+    }
+
+    @Test
+    public void getBookings_with_firstname_filter_test() {
+
+        int bookingId = createBookingId("Melda", "Celik", 10, false);
+
+        spec.queryParam("firstname", "Melda");
+
+        Response response = given(spec)
+                .when()
+                .get("/booking");
+
+        response
+                .then()
+                .statusCode(200);
+
+        List<Integer> list = response.jsonPath().getList("bookingid");
+        Assertions.assertTrue(list.contains(bookingId));
 
     }
 
